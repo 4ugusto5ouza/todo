cd7f709b256b2146a8323fba117b70bc1c65c9d7
Samuel Souza <augusto_souza@edu.unifil.br>
2022-05-02 15:17:59 -0300

    Criado base para App TODO+

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
+
+const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("clicou");
+  };
+
   return (
     <div className="App">
-      <h1>Todo List</h1>
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+      <div className="form-todo">
+        <h2>Insira sua tarefa:</h2>
+        <form action="" onSubmit={handleSubmit}>
+          <input type="submit" value="Enviar" />
+        </form>
+      </div>
+      <div className="list-todo">
+        <h2>Lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+      </div>
     </div>
   );
 }
